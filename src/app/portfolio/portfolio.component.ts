8fb878f9b21a1646c4526efd9c43362f85a02a5e
Hassan <hassan.farahat@su.edu.eg>
2024-09-06 21:02:01 +0300

    hide model

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,5 +9,36 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  ishide:boolean=true;
+  modelImage :string='';
+  img:string='';
+
+  assetimage:string[]=[
+    'assets/images/poert1.png',
+     'assets/images/port2.png',
+      'assets/images/port3.png',
+      'assets/images/port2.png',
+      'assets/images/poert1.png',
+       'assets/images/port2.png'
+
+  ]
+
   color:string='#2C3E50';
+
+  hideModel(elevent:EventTarget |null,imaghref:HTMLImageElement):void
+
+{
+
+  console.log(elevent);
+  console.log(imaghref);
+  
+  
+
+  if(elevent==imaghref)return ;
+
+  else
+  this.ishide=true
+
+}
+
 }
